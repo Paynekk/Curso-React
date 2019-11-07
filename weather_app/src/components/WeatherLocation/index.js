9c745212b09cd0182d7113e4a142a1e59e611685
Miguel Angel Preciado <kemse01@gmail.com>
2019-11-07 00:04:24 -0600

    '

--- weather_app/src/components/WeatherLocation/index.js
+++ weather_app/src/components/WeatherLocation/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Location from './Location'
 import WeatherData from './WeatherData'
 import './styles.css'
 import {
-  CLOUD, 
-  CLOUDY,
   SUN,
-  RAIN, 
-  SNOW,
-  WINDY, 
+  WINDY
   }from './../../constants/weathers'
   const data = {
     temperature: 5,
@@ -16,11 +12,42 @@ import {
     humidity: 10,
     wind: '10 m/s'
   }
-const WeatherLocation =  () => (
-  <div className="weatherLocationCont">
-  <Location city={"Mexico"}></Location>
-  <WeatherData data={data}></WeatherData>
-  </div>
-);
+
+  const data2 = {
+    temperature: 5,
+    weatherState: WINDY,
+    humidity: 10,
+    wind: '10 m/s'
+  }
+class WeatherLocation extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      city: 'Buenos Aires',
+      data: data,
+    };
+  }
+
+  handleUpdateClick = () => {
+    console.log("actualizado");
+    this.setState({
+      data: data2
+    })
+  }
+
+  render(){
+    const {city, data} = this.state;
+    return(
+    <div className="weatherLocationCont">
+    <Location city={/* this.state.city */ city}></Location>
+    <WeatherData data={data}></WeatherData>
+    <button onClick={this.handleUpdateClick}>Actualizar</button>
+    </div>);
+  }
+  
+}
+  
+
 
 export default WeatherLocation;
