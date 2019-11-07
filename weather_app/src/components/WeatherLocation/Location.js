9e3e2deed103b8d69c6f9be6f553d21dde223dd1
Miguel Angel Preciado <kemse01@gmail.com>
2019-11-06 18:37:02 -0600

    componentes

--- weather_app/src/components/WeatherLocation/Location.js
+++ weather_app/src/components/WeatherLocation/Location.js
@@ -1,14 +1,17 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
+import './styles.css'
 const Location = (/*props*/{city}) => (
 // destructuring
 // const city = props.city mientras tenga el mismo nombre  puede hacerse destructuring
   /* const {city} = props */
-  <div>
+  <div className="locationCont">
   <h1>{city}</h1>
   </div>
 )
     
 
-
+Location.propTypes = {
+  city: PropTypes.string.isRequired,
+}
 export default Location;
